8f0f9d48e5ee86609dfcf05c9fc9dcb7f318d15d
kobrineli <kobrineli@ispras.ru>
2022-10-18 16:44:23 +0300

    [04] Rewrote with arrays.

--- 04_Text/randomize.sh
+++ 04_Text/randomize.sh
@@ -1,56 +1,37 @@
 #!/bin/bash
 
-exit_handler() {
-    trap - EXIT
-    test -r "$TMPFILE" && rm -f "$TMPFILE"
-    test -r "$POSFILE" && rm -f "$POSFILE"
-}
-
-trap exit_handler EXIT
-
 DELAY=0
 if [[ $# != 0 ]]
 then
     DELAY=$1
 fi
 
-TMPFILE="`mktemp`"
-while IFS= read -r VAR;
+lines=()
+poses=()
+cur=1
+while IFS= read -r line;
 do
-    echo -e "$VAR"
-    echo -e "$VAR" >> $TMPFILE
-done
-
-LINES="$(cat $TMPFILE | wc -l)"
-
-POSFILE="`mktemp`"
-for i in $(seq 1 $LINES);
-do
-    LINE=$(sed -n "${i}p" $TMPFILE)
-    for j in $(seq 1 ${#LINE});
+    lines+=("$line")
+    for j in $(seq 1 ${#line});
     do
-        echo "$i $j" >> $POSFILE
+        poses+=("$cur.$j")
     done
+    cur=$((cur + 1))
 done
-
-#for pos in "$(shuf $POSFILE)"
-#do
-#    echo "${positions[0]} "
-#done
 
 tput clear
 
-while IFS= read -r pos
-do
+for pos in $(shuf -e "${poses[@]}"); do
     sleep $DELAY
-    position=($pos)
-    line_idx=${position[0]}
-    col_idx=${position[1]}
+    line_idx=$(echo $pos | cut -d '.' -f 1)
+    col_idx=$(echo $pos | cut -d '.' -f 2)
 
-    LINE=$(sed -n "${line_idx}p" $TMPFILE)
-
-    tput cup $((line_idx - 1)) $((col_idx - 1))
-    echo -n -e "${LINE:$((col_idx - 1)):${col_idx}}"
-done < <(shuf $POSFILE)
+    symbol=${lines[(($line_idx - 1))]:$((col_idx - 1)):1}
+    if [[ $symbol != " " ]]
+    then
+        tput cup $((line_idx - 1)) $((col_idx - 1))
+        printf "$symbol"
+    fi
+done
 
 tput cup $(($(tput lines) - 1)) 0
